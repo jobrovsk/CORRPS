b1595433f5dfd71c892e72127e74aa7d94bddfc1
Jakob222 <91540477+Jakob222@users.noreply.github.com>
2026-01-10 15:10:15 +0100

    Update RPiSigmaRingReduction.m

--- RPiSigmaRingReduction.m
+++ RPiSigmaRingReduction.m
@@ -1,6 +1,8 @@
 (* ::Package:: *)
 
- 
+$activateEcho=False;
+Clear[JEcho];
+JEcho[args___]:=If[$activateEcho,Echo[args],args]
 
 
 Clear[RationalReduction]
@@ -12,16 +14,30 @@ Return[eta^(-1) RationalReductionSigmaReduced[eta g,xi,tower]]
 
 
 Clear[RationalReductionSigmaReduced]
-RationalReductionSigmaReduced[g_,xi_,tower_]:=Module[{},
-
+RationalReductionSigmaReduced[g_,xi_,tower_]:=Module[{gT,h},
+{h,gT}=ProperAndPolynomialParts[f,var];
 s
 
 ]
 
 
 Clear[NormalForm]
-NormalForm[f_,tower_]:=Module[{xi,eta},
+NormalForm[f_,tower_]:=Module[{xi,eta,},
 
 
 Return[{xi,eta}]
 ]
+
+
+(* ::Subsection:: *)
+(*Rational*)
+
+
+(* ::Input::Initialization:: *)
+Clear[ProperAndPolynomialParts];
+ProperAndPolynomialParts[f_,var_Symbol]:=Module[{ff,num,den,r,pp,fp},
+ff=Together[f];
+{num,den}=NumeratorDenominator[ff];
+{pp,r}=PolynomialQuotientRemainder[num,den,var];
+Return[{r/den,pp}]
+];
