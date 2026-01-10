a2ba108dd484e4abe6be833b58768961661cd3d2
Jakob222 <91540477+Jakob222@users.noreply.github.com>
2026-01-10 12:35:11 +0100

    Rational.m by Yiman

--- RPiSigmaRingReduction.m
+++ RPiSigmaRingReduction.m
@@ -1,3 +1,27 @@
 (* ::Package:: *)
 
+ 
 
+
+Clear[RationalReduction]
+RationalReduction[g_,f_,tower_]:=Module[{xi,eta},
+{xi,eta}=NormalForm[f,tower];
+
+Return[eta^(-1) RationalReductionSigmaReduced[eta g,xi,tower]]
+]
+
+
+Clear[RationalReductionSigmaReduced]
+RationalReductionSigmaReduced[g_,xi_,tower_]:=Module[{},
+
+s
+
+]
+
+
+Clear[NormalForm]
+NormalForm[f_,tower_]:=Module[{xi,eta},
+
+
+Return[{xi,eta}]
+]
